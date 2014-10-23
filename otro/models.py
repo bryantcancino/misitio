397e4c258af5bc1bcf43c7a3910024c701f72000
bryantcancino <zztopfenck@hotmail.com>
2014-10-22 19:24:42 -0500

    primera parte del proyecto
    proyecto para el V congreso de ingenieria

---
+++ otro/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class otro(object):
+	nombre = models.CharField(max_length = 100, primary_key = True)
+	portada = models.ImageField(upload_to = 'static')
+	precio = models.CharField(max_length = 500)
+
+
+	def __init__(self):
+		return self.nombre
---
+++ pelicula/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class pelicula(object):
+	nombre = models.CharField(max_length = 100, primary_key = True)
+	portada = models.ImageField(upload_to = 'static')
+	precio = models.CharField(max_length = 500)
+
+
+	def __init__(self):
+		return self.nombre
+		
